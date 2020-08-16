b4ce632c19bda8a6f8af69a042cf343c92d8018b
Willow Key <Mercury9791@gmail.com>
2020-08-16 13:21:15 -0700

    Improvements to PlayerController

---
+++ Snakoban/Assets/Scripts/CrateController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrateController : MonoBehaviour
+{
+
+
+    // Detect objects in a given direction.
+    public bool LegalMove(Vector2 direction)
+    {
+        bool result = true;
+
+        Vector2 pos = new Vector2(transform.position.x, transform.position.y);
+        RaycastHit2D hit = Physics2D.Linecast(pos, pos + direction);
+
+        if (hit.transform.tag == "Wall")
+        {
+            result = false;
+        }
+        else if (hit.transform.tag == "Crate")
+        {
+            // hit.transform.gameObject.
+        }
+
+        return result;
+        ;
+    }
+}
--- Snakoban/Assets/Scripts/PlayerController.cs
+++ Snakoban/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Input Detection and movement.
         if (Input.GetButtonDown("Up"))
         {
             transform.Translate(Vector2.up);
@@ -25,5 +26,25 @@ public class PlayerController : MonoBehaviour
             transform.Translate(Vector2.left);
         }
 
+    }
+
+    // Detect objects in a given direction.
+    public bool LegalMove(Vector2 direction)
+    {
+        bool result = true;
+
+        Vector2 pos = new Vector2(transform.position.x, transform.position.y);
+        RaycastHit2D hit = Physics2D.Linecast(pos, pos + direction);
+
+        if (hit.transform.tag == "Wall")
+        {
+            result = false;
+        } else if (hit.transform.tag == "Crate")
+        {
+            result = hit.transform.gameObject.GetComponent<CrateController>().LegalMove(direction);
+        }
+
+        return result;
+        ;
     }
 }
