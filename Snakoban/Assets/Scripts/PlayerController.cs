64af95eaf4f91e9fa9442d50cf9d45d66df87e5e
Willow Key <WillowKey@berkeley.edu>
2020-08-16 09:45:28 -0700

    Primitive Player Controller
    A basic controller that translates objects by 1 tile increment when arrow or 'wasd' keys are pressed.

---
+++ Snakoban/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Up"))
+        {
+            transform.Translate(Vector2.up);
+        } else if (Input.GetButtonDown("Down"))
+        {
+            transform.Translate(Vector2.down);
+        }
+        else if (Input.GetButtonDown("Right"))
+        {
+            transform.Translate(Vector2.right);
+        }
+        else if (Input.GetButtonDown("Left"))
+        {
+            transform.Translate(Vector2.left);
+        }
+
+    }
+}
